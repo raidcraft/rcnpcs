4df965386350de3196456d7eef5acc08472e3269
Michael Reichenbach <michael@mirenoba.com>
2019-02-03 17:46:23 +0100

    feat(trait): use YamlDataKey id as conversation and tof trait id

--- src/main/java/de/raidcraft/npcs/traits/ToFNPCTrait.java
+++ src/main/java/de/raidcraft/npcs/traits/ToFNPCTrait.java
@@ -1,10 +1,13 @@
 package de.raidcraft.npcs.traits;
 
+import de.raidcraft.npcs.ConfigStorage;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import net.citizensnpcs.api.exception.NPCLoadException;
 import net.citizensnpcs.api.persistence.Persist;
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.trait.TraitName;
+import net.citizensnpcs.api.util.DataKey;
 
 @TraitName("ToF")
 @Data
@@ -16,5 +19,12 @@ public class ToFNPCTrait extends Trait {
 
     public ToFNPCTrait() {
         super("ToF");
+    }
+
+    @Override
+    public void load(DataKey key) throws NPCLoadException {
+        if (key instanceof ConfigStorage.YamlKey) {
+            this.configPath = ((ConfigStorage.YamlKey) key).getId();
+        }
     }
 }
